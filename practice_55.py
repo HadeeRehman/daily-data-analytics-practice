6bca9c40d90f6943f5c6ea6897d0d06bbd579cf2
HadeeRehman <parrayhadi1223@gmail.com>
2026-08-29 23:34:11 +0530

    Plot polynomial data across 2x2 subplots
    Created a 2x2 subplot layout to display linear, quadratic, cubic, and quartic transformations using line, bar, and scatter charts with customized titles and tight layout spacing.

---
+++ practice_55.py
@@ -0,0 +1,27 @@
+# Plot polynomial transformations across a 2x2 subplot grid in Matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+print(plt.subplots(2, 2)) 
+x = np.array([1, 2, 3, 4, 5])
+
+figure, axes = plt.subplots(2, 2) 
+
+axes[0, 0].plot(x, x*2,  
+                color='red')
+axes[0, 0].set_title("x*2")
+axes[0, 0].set_xlabel('marks')
+axes[0, 0].set_ylabel('studetns')
+
+axes[0, 1].bar(x, x**2, color='blue')
+axes[0, 1].set_title("x**2")
+
+axes[1, 0].plot(x, x**3, color='purple')
+axes[1, 0].set_title("x**3")
+
+axes[1, 1].scatter(x, x**4, color='green')
+axes[1, 1].set_title("x**4")
+
+
+plt.tight_layout()
+plt.show()
